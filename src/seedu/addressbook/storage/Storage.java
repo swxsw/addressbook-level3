20d7794193904f4883cb948939ba1b7a062ab5f9
swxsw <suwei9@gmail.com>
2016-09-25 01:35:31 +0800

    Added comments to Storage interface
    Added comments to Storage interface

--- src/seedu/addressbook/storage/Storage.java
+++ src/seedu/addressbook/storage/Storage.java
@@ -1,15 +1,18 @@
 package seedu.addressbook.storage;
 
-import java.nio.file.Path;
 
 import seedu.addressbook.data.AddressBook;
 import seedu.addressbook.storage.StorageFile.StorageOperationException;
+
+/**
+ * Specifies the methods required for an object to represent the file used to store address book data.
+ */
 
 public interface Storage {
     
     /** Default file path used if the user doesn't provide the file name. */
     public static final String DEFAULT_STORAGE_FILEPATH = "addressbook.txt";
-
+    
     public String getPath();
 
     public void save(AddressBook addressBook) throws StorageOperationException;
